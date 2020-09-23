afd79d3eb161f1f288c513215aae1291a1a63460
Edwin Sue <edwinsue92@gmail.com>
2020-09-23 16:45:02 -0400

    question 2

--- Galvanize Learn.sql
+++ Galvanize Learn.sql
@@ -1,1 +1,7 @@
---Taxi Trip Database
+SELECT 
+ROUND ((trip_distance / ROUND((CAST((EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime))/3600) AS NUMERIC)), 2)), 2) AS mph
+FROM taxi_trips
+WHERE ROUND((CAST((EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime))/3600) AS NUMERIC)), 2) != 0
+
+
+--## ROUND function not working for me, not due to syntax error
