7b744ccf4698d138d70d18b42ccb73aafbbc54cd
Edwin Sue <edwinsue92@gmail.com>
2020-09-23 16:52:00 -0400

    question 3

--- Galvanize Learn.sql
+++ Galvanize Learn.sql
@@ -1,7 +1,11 @@
+--##Average Speed of Driver per Trip
 SELECT 
 ROUND ((trip_distance / ROUND((CAST((EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime))/3600) AS NUMERIC)), 2)), 2) AS mph
 FROM taxi_trips
 WHERE ROUND((CAST((EXTRACT(EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime))/3600) AS NUMERIC)), 2) != 0
 
-
---## ROUND function not working for me, not due to syntax error
+--##Average Cost/Rider for Multi-Rider trips
+SELECT
+ROUND((SUM(total_amount) / SUM(passenger_count)), 2) AS avg_cost
+FROM taxi_trips
+WHERE passenger_count > 1 
