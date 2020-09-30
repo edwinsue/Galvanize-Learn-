dcc778f09f6fafad895f7dda2c41aa9c24c5433a
Edwin Sue <edwinsue92@gmail.com>
2020-09-30 16:39:00 -0400

    Question 4

--- PGexercises_2.sql
+++ PGexercises_2.sql
@@ -8,49 +8,43 @@ AND m.surname LIKE 'Farrell'
 ORDER BY 1
 
 --##2
-SELECT * FROM facilities
+SELECT 
+firstname,
+surname
+FROM members 
+WHERE memid IN (SELECT DISTINCT recommendedby FROM members)
+ORDER BY 2
 
 --##3
-SELECT * FROM bookings
+SELECT
+m1.firstname AS member_first,
+m1.surname AS member_last,
+m2.firstname AS recommender_first,
+m2.surname AS recommender_last
+FROM members m1
+LEFT JOIN members m2 ON m1.recommendedby = m2.memid
+ORDER BY 2,1
 
 --##4
-SELECT
-name,
-membercost
-FROM facilities
+SELECT DISTINCT
+m.firstname AS firstname,
+m.surname AS surname,
+f.name AS name
+FROM bookings b
+LEFT JOIN facilities f ON b.facid = f.facid
+LEFT JOIN members m ON b.memid = m.memid
+WHERE f.name LIKE '%Tennis%Court%'
+ORDER BY 1,2
 
 --##5
-SELECT
-name,
-membercost
-FROM facilities
-WHERE membercost > 0 
+
 
 --##6
-SELECT
-facid, 
-name, 
-membercost, 
-monthlymaintenance
-FROM facilities
-WHERE membercost > 0 
-AND membercost < (0.02 * monthlymaintenance)
+
  
 --##7
-SELECT
-*
-FROM facilities
-WHERE name LIKE '%Tennis%'
 
 --##8
-SELECT
-MAX(joindate)
-FROM members
 
 --##9
-SELECT
-firstname,
-surname,
-joindate
-FROM members
-WHERE joindate = (SELECT MAX(joindate) FROM members)
+
